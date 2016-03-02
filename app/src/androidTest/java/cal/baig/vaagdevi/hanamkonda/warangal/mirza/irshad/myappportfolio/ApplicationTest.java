a4d85a276e654ac554a20270759c5b1066833334
Irshad Mirza <irshadmirza@Irshads-MacBook-Air.local>
2016-03-02 12:37:18 +0530

    Initial commit on 02 March 2016 at 12:37pm

---
+++ app/src/androidTest/java/cal/baig/vaagdevi/hanamkonda/warangal/mirza/irshad/myappportfolio/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cal.baig.vaagdevi.hanamkonda.warangal.mirza.irshad.myappportfolio;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
